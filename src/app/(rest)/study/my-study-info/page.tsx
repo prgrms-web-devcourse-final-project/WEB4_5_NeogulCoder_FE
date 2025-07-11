cc375beeace6894e1e07a5ff59a6f8a9e4a54101
yubin121 <yubin045@naver.com>
2025-07-11 15:43:08 +0900

    ✨ Feat: 스터디 공간 정보 페이지 구현

---
+++ src/app/(rest)/study/my-study-info/page.tsx
@@ -0,0 +1,81 @@
+'use client';
+
+import ListMenu from '@/components/my/ListMenu';
+import RecruitmentCard from '@/components/my/RecruitmentCard';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+
+export default function MyStudyInfo() {
+  const recruitmentType = {
+    title: '오늘 스터디 내용 정리본입니당',
+    content:
+      '국회는 의원의 자격을 심사하며, 의원을 징계할 수 있다. 국무회의는 대통령·국무총리와 15인 이상 30인 이하의 국무위원으로 구성한다. 국회는 헌법 또는 법률에 특별한 규정이 없는 한 나나나나나나나나나',
+    createdAt: '2025.07.01',
+    commentCount: 3,
+    status: '자유',
+    type: 'study',
+  };
+
+  return (
+    // py-15
+    <div className="w-full px-6">
+      <div className="tb1 text-[22px] text-text1">스터디 My 정보</div>
+      <div className="tm1 text-[20px] text-text1 mt-[30px]">
+        내가 작성한 모집 글
+      </div>
+      <div className="mt-6">
+        <ListMenu />
+      </div>
+      {/* flex justify-between */}
+      <div className="flex flex-col gap-[30px] mt-[30px]">
+        <RecruitmentCard {...recruitmentType} />
+        <RecruitmentCard {...recruitmentType} />
+        <RecruitmentCard {...recruitmentType} />
+        <RecruitmentCard {...recruitmentType} />
+        <RecruitmentCard {...recruitmentType} />
+        <div className="flex gap-5 justify-center items-center">
+          <ChevronLeft className="w-6 h-6 cursor-pointer" />
+          <div className="flex items-center gap-3 tl2 text-text1">
+            <button className="flex justify-center items-center bg-main text-white w-6 h-6 rounded-[50%]">
+              1
+            </button>
+            <button className="flex justify-center items-center bg-none text-text1 w-6 h-6 rounded-[50%]">
+              2
+            </button>
+            <button className="flex justify-center items-center bg-none text-text1 w-6 h-6 rounded-[50%]">
+              3
+            </button>
+            <button className="flex justify-center items-center bg-none text-text1 w-6 h-6 rounded-[50%]">
+              4
+            </button>
+            <button className="flex justify-center items-center bg-none text-text1 w-6 h-6 rounded-[50%]">
+              5
+            </button>
+          </div>
+          <ChevronRight className="w-6 h-6 cursor-pointer" />
+        </div>
+      </div>
+      <div className="mt-13">
+        <div className="tm1 text-[20px] text-text1">스터디 연장</div>
+        <hr className="mt-4 text-border2" />
+        <div className="flex justify-between items-center mt-6 t2">
+          <span className="text-red">스터디를 연장하실 수 있습니다.</span>
+          <button className="w-[144px] h-[48px] bg-main rounded-[10px] text-white">
+            스터디 연장하기
+          </button>
+        </div>
+      </div>
+      <div className="mt-13">
+        <div className="tm1 text-[20px] text-text1">스터디 탈퇴</div>
+        <hr className="mt-4 text-border2" />
+        <div className="flex justify-between items-center mt-6 t2">
+          <span className="text-red">
+            스터디를 탈퇴하면 불이익이 발생합니다.
+          </span>
+          <button className="w-[144px] h-[48px] bg-main rounded-[10px] text-white">
+            스터디 탈퇴하기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
