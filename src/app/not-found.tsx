bf5e23807328dfa2e6aa93ece2663bab3405f60d
sduddla <sduddla921@gmail.com>
2025-07-13 00:38:20 +0900

    💄 Style: 404 페이지 퍼블리싱 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,6 +11,7 @@ export default function NotFound() {
   return (
     <div className="flex flex-col justify-center items-center gap-12 min-h-screen">
       <Image src={notFound} alt="404" width={600} />
+      <p>페이지를 찾을 수 없습니다.</p>
       <button
         type="button"
         className="button-type1 cursor-pointer"
