5702468287d7388b421153857423350c5e57178d
yubin121 <yubin045@naver.com>
2025-07-17 17:28:23 +0900

    ✨ Feat: 평가 완료 상태 추가 및 로직 수정

--- src/components/my/MannersClient.tsx
+++ src/components/my/MannersClient.tsx
@@ -6,7 +6,9 @@ import MannerTooltip from '@/components/my/MannerTooltip';
 import SubMenuItem from '@/components/my/SubMenuItem';
 import { fetchStudyList, fetchUserListByStudyId } from '@/lib/api/manners';
 import { BadgeQuestionMark } from 'lucide-react';
-import { useState } from 'react';
+import Image from 'next/image';
+import completeStamp from '@/assets/images/complete-stamp.svg';
+import { useEffect, useState } from 'react';
 
 export default function MannersClient({
   initialStudyList,
@@ -21,17 +23,17 @@ export default function MannersClient({
   const [activeStudyIndex, setActiveStudyIndex] = useState(0);
   const [activeUserIndex, setActiveUserIndex] = useState(0);
 
-  const [studyList, setStudyList] = useState<Study[]>(initialStudyList);
-  const [userList, setUserList] = useState<User[]>(initialUserList);
+  const [studyList, setStudyList] = useState<Study[]>(initialStudyList ?? []);
+  const [userList, setUserList] = useState<User[]>(initialUserList ?? []);
 
   const [currentStudyId, setCurrentStudyId] = useState<number>(
     initialStudyId ?? -1
   );
   const [currentUserId, setCurrentUserId] = useState<number>(
-    initialUserList[0].userId ?? -1
+    initialUserList?.length > 0 ? initialUserList[0].userId : -1
   );
   const [currentUserName, setCurrentUserName] = useState<string>(
-    initialUserList[0].nickname
+    initialUserList?.length > 0 ? initialUserList[0].nickname : ''
   );
 
   const getStudyList = async () => {
@@ -78,8 +80,18 @@ export default function MannersClient({
     setCurrentUserName(nickname);
   };
 
+  const [showAnimation, setShowAnimation] = useState(false);
+
+  useEffect(() => {
+    setShowAnimation(true);
+  }, []);
+
   return (
-    <div className='w-full max-w-[908px] relative'>
+    <div
+      className={`w-full max-w-[908px] relative ${
+        studyList.length === 0 && 'h-[calc(100vh-105px-113px)]'
+      }`}
+    >
       {isShown && (
         <div className='absolute top-9 right-1 z-10'>
           <MannerTooltip />
@@ -93,42 +105,63 @@ export default function MannersClient({
           onMouseLeave={() => setIsShown(false)}
         />
       </div>
-      <div className='max-w-[908px] overflow-hidden'>
-        <div className='overflow-x-auto scroll-custom'>
-          <div className='flex items-center gap-10 mt-6'>
-            {studyList.map((study, i) => (
-              <MannerStudyItem
-                key={`${study.studyId} + ${i}`}
-                {...study}
-                isActive={i === activeStudyIndex}
-                onClick={() => handleStudyClick(study.studyId, i)}
-              />
-            ))}
-          </div>
+      {studyList.length === 0 ? (
+        <div className='flex flex-col gap-10 justify-center items-center absolute top-[45%] left-1/2 -translate-x-1/2 -translate-y-1/2 tm2 text-text1/80'>
+          <Image
+            src={completeStamp}
+            alt='completeStamp'
+            className={`w-28 h-28 -rotate-[20deg] ${
+              showAnimation ? 'animate-stamp' : 'opacity-0'
+            }`}
+          />
+          <span>현재 가능한 매너 평가가 없습니다!</span>
         </div>
-      </div>
-      <div className='max-w-[908px] overflow-hidden'>
-        <div className='overflow-x-auto scroll-custom'>
-          <div className='flex items-center mt-6'>
-            {userList.map((user, i) => (
-              <SubMenuItem
-                key={`${user.userId} - ${i}`}
-                isActive={i === activeUserIndex}
-                onClick={() => handleUserClick(user.userId, user.nickname, i)}
-              >
-                {user.nickname}
-              </SubMenuItem>
-            ))}
+      ) : (
+        <>
+          <div className='max-w-[908px] overflow-hidden'>
+            <div className='overflow-x-auto scroll-custom'>
+              <div className='flex items-center gap-10 mt-6'>
+                {Array.isArray(studyList) &&
+                  studyList &&
+                  studyList.map((study, i) => (
+                    <MannerStudyItem
+                      key={`${study.studyId} - ${i}`}
+                      {...study}
+                      isActive={i === activeStudyIndex}
+                      onClick={() => handleStudyClick(study.studyId, i)}
+                    />
+                  ))}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <MannerQuestion
-        key={`${activeStudyIndex}-${activeUserIndex}`}
-        currentStudyId={currentStudyId}
-        currentUserId={currentUserId}
-        currentUserName={currentUserName}
-        getUserList={getUserList}
-      />
+          <div className='max-w-[908px] overflow-hidden'>
+            <div className='overflow-x-auto scroll-custom'>
+              <div className='flex items-center mt-6'>
+                {Array.isArray(userList) &&
+                  userList &&
+                  userList.map((user, i) => (
+                    <SubMenuItem
+                      key={`${user.userId} - ${i}`}
+                      isActive={i === activeUserIndex}
+                      onClick={() =>
+                        handleUserClick(user.userId, user.nickname, i)
+                      }
+                    >
+                      {user.nickname}
+                    </SubMenuItem>
+                  ))}
+              </div>
+            </div>
+          </div>
+          <MannerQuestion
+            key={`${activeStudyIndex}-${activeUserIndex}`}
+            currentStudyId={currentStudyId}
+            currentUserId={currentUserId}
+            currentUserName={currentUserName}
+            getUserList={getUserList}
+          />
+        </>
+      )}
     </div>
   );
 }
