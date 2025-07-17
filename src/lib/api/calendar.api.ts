f5326ba4dbc4b2bec7bcf0607d97671b8c25f0d5
Chosojeong <chosojeong97@gmail.com>
2025-07-17 23:53:53 +0900

    🐛 Fix : api수정

--- src/lib/api/calendar.api.ts
+++ src/lib/api/calendar.api.ts
@@ -1,91 +1,109 @@
-import axios from 'axios';
-
-export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-});
+import { axiosInstance } from './axios';
 
 // 내 스터디 목록
-export const getStudies = () => {
-  return axiosInstance.get('/api/studies');
+export const getStudies = async () => {
+  const { data } = await axiosInstance.get('/api/studies');
+  return data;
 };
 
 // 특정 스터디 조회
-export const getStudy = (studyId: number) => {
-  return axiosInstance.get(`/api/studies/${studyId}`);
+export const getStudy = async (studyId: number) => {
+  const { data } = await axiosInstance.get(`/api/studies/${studyId}`);
+  return data;
 };
 
 // 스터디별 일정 전체 목록 조회
-export const getStudyEvents = (studyId: number) => {
-  return axiosInstance.get(`/api/teams/${studyId}/calendar`);
+export const getStudyEvents = async (studyId: number) => {
+  const { data } = await axiosInstance.get(`/api/teams/${studyId}/calendar`);
+  return data;
 };
 
 // 스터디 일정 날짜별 조회
-export const getStudyDayEvents = (studyId: number, viewDate: string) => {
-  return axiosInstance.get(
+export const getStudyDayEvents = async (studyId: number, viewDate: string) => {
+  const { data } = await axiosInstance.get(
     `/api/teams/${studyId}/calendar/day?date=${viewDate}`
   );
+  return data;
 };
 
 // 스터디팀 일정 등록
-export const postStudyEvent = (
+export const postStudyEvent = async (
   studyId: number,
   updateData: ScheduleInputType
 ) => {
-  return axiosInstance.post(`/api/teams/${studyId}/calendar`, updateData);
+  const { data } = await axiosInstance.post(
+    `/api/teams/${studyId}/calendar`,
+    updateData
+  );
+  return data;
 };
 
 // 스터디팀 일정 수정
-export const putStudyEvent = (
+export const putStudyEvent = async (
   studyId: number,
   calendarId: number,
   updateData: ScheduleInputType
 ) => {
-  return axiosInstance.put(
+  const { data } = await axiosInstance.put(
     `/api/teams/${studyId}/calendar/${calendarId}`,
     updateData
   );
+  return data;
 };
 
 // 스터디팀 일정 삭제
-export const deleteStudyEvent = (studyId: number, calendarId: number) => {
-  return axiosInstance.delete(`/api/teams/${studyId}/calendar/${calendarId}`);
+export const deleteStudyEvent = async (studyId: number, calendarId: number) => {
+  const { data } = await axiosInstance.delete(
+    `/api/teams/${studyId}/calendar/${calendarId}`
+  );
+  return data;
 };
 
 ///////////////////개인 일정//////////////////
 
 // 개인 일정 전체 조회
-export const getUserEvents = (userId: number) => {
-  return axiosInstance.get(`/api/users/${userId}/calendar`);
+export const getUserEvents = async (userId: number) => {
+  const { data } = await axiosInstance.get(`/api/users/${userId}/calendar`);
+  return data;
 };
 
 // 개인 일정 날짜별 조회
-export const getUserDayEvents = (userId: number, viewDate: string) => {
-  return axiosInstance.get(
+export const getUserDayEvents = async (userId: number, viewDate: string) => {
+  const { data } = await axiosInstance.get(
     `/api/users/${userId}/calendar/day?date=${viewDate}`
   );
+  return data;
 };
 
 // 스터디팀 일정 등록
-export const postUserEvent = (
+export const postUserEvent = async (
   userId: number,
   updateData: UserScheduleInputType
 ) => {
-  return axiosInstance.post(`/api/users/${userId}/calendar`, updateData);
+  const { data } = await axiosInstance.post(
+    `/api/users/${userId}/calendar`,
+    updateData
+  );
+  return data;
 };
 
 // 개인 일정 수정
-export const putUserEvent = (
+export const putUserEvent = async (
   userId: number,
   calendarId: number,
   updateData: UserScheduleInputType
 ) => {
-  return axiosInstance.put(
+  const { data } = await axiosInstance.put(
     `/api/users/${userId}/calendar/${calendarId}`,
     updateData
   );
+  return data;
 };
 
 // 개인 일정 삭제
-export const deleteUserEvent = (userId: number, calendarId: number) => {
-  return axiosInstance.delete(`/api/users/${userId}/calendar/${calendarId}`);
+export const deleteUserEvent = async (userId: number, calendarId: number) => {
+  const { data } = await axiosInstance.delete(
+    `/api/users/${userId}/calendar/${calendarId}`
+  );
+  return data;
 };
--- src/lib/api/study.api.ts
+++ src/lib/api/study.api.ts
@@ -1,15 +1,13 @@
-import axios from 'axios';
-
-export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-});
+import { axiosInstance } from './axios';
 
 // 내 스터디 목록
-export const getStudyInfoData = (studyId: number) => {
-  return axiosInstance.get(`/api/studies/${studyId}/info`);
+export const getStudyInfoData = async (studyId: number) => {
+  const { data } = await axiosInstance.get(`/api/studies/${studyId}/info`);
+  return data;
 };
 
 // 특정 스터디 조회
-export const getStudyHeaderData = (studyId: number) => {
-  return axiosInstance.get(`/api/studies/${studyId}/header`);
+export const getStudyHeaderData = async (studyId: number) => {
+  const { data } = await axiosInstance.get(`/api/studies/${studyId}/header`);
+  return data;
 };
