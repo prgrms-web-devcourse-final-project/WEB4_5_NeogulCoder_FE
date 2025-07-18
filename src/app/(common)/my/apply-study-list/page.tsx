4ca0ecf04142a6cb88dec0c809c4400b8c0ade7f
yubin121 <yubin045@naver.com>
2025-07-18 14:43:00 +0900

    🐛 Fix: 신청 스터디 목록 구조 수정

--- src/app/(common)/my/apply-study-list/page.tsx
+++ src/app/(common)/my/apply-study-list/page.tsx
@@ -1,54 +1,53 @@
-import Pagination from '@/components/common/Pagination';
-import ApplyingStudyCard from '@/components/my/ApplyingStudyCard';
-import ListMenu from '@/components/my/ListMenu';
+import ApplicationListClient from '@/components/my/ApplicationListClient';
 
 export default function ApplyStudyList() {
-  const studyType = {
-    studyName: '알고리즘 알고가자',
-    studyLeader: '한유빙',
-    currentMemberCount: 5,
-    totalMemberCount: 6,
-    startDate: '2025.06.24',
-    studyIntro: '알고리즘은 알고 가야 하지 않겠니',
-    category: 'IT',
-    studyWay: '온라인',
+  const responseType = {
+    applications: [
+      {
+        name: '자바 스터디',
+        leaderNickname: '너굴',
+        capacity: 4,
+        currentCount: 3,
+        startDate: '2025-07-15',
+        imageUrl: '',
+        introduction: '자바 스터디입니다.',
+        category: 'IT',
+        studyType: 'ONLINE',
+        status: 'PENDING',
+        read: true,
+      },
+      {
+        name: '자바 스터디',
+        leaderNickname: '너굴',
+        capacity: 4,
+        currentCount: 3,
+        startDate: '2025-07-15',
+        imageUrl: '',
+        introduction: '자바 스터디입니다.',
+        category: 'IT',
+        studyType: 'ONLINE',
+        status: 'PENDING',
+        read: true,
+      },
+      {
+        name: '자바 스터디',
+        leaderNickname: '너굴',
+        capacity: 4,
+        currentCount: 3,
+        startDate: '2025-07-15',
+        imageUrl: '',
+        introduction: '자바 스터디입니다.',
+        category: 'IT',
+        studyType: 'ONLINE',
+        status: 'PENDING',
+        read: true,
+      },
+    ],
+    totalPage: 2,
+    totalElementCount: 10,
   };
 
-  return (
-    <div className='w-full'>
-      <div className='tb3'>내가 신청한 스터디</div>
-      <div className='mt-6'>
-        <ListMenu />
-      </div>
-      <div className='grid grid-cols-3 gap-[26px] mt-[30px]'>
-        {[...Array(4)].map((_, i) => (
-          <ApplyingStudyCard
-            key={i}
-            {...studyType}
-            isShown={false}
-            status='미완료'
-          />
-        ))}
-        {[...Array(4)].map((_, i) => (
-          <ApplyingStudyCard
-            key={i}
-            {...studyType}
-            isShown={true}
-            status='승인'
-          />
-        ))}
-        {[...Array(4)].map((_, i) => (
-          <ApplyingStudyCard
-            key={i}
-            {...studyType}
-            isShown={true}
-            status='거절'
-          />
-        ))}
-      </div>
-      <div className='mt-[30px]'>
-        <Pagination />
-      </div>
-    </div>
-  );
+  const applicationList = responseType;
+
+  return <ApplicationListClient applicationList={applicationList} />;
 }
