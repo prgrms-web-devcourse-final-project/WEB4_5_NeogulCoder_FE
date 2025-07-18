60929e87381c9cb4bd5831ec8a9b429e6d51e736
sduddla <sduddla921@gmail.com>
2025-07-18 10:03:36 +0900

    ✨ Feat: 로그인/회원가입 & 사용자 정보

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang='ko'>
       <body>
-        {/* <UserStore /> */}
+        <UserStore />
         {children}
       </body>
     </html>
