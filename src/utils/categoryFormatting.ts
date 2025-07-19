3f4c6c1616a87d7f9edc40ed7379f543c88c9f7d
Chosojeong <chosojeong97@gmail.com>
2025-07-20 00:19:35 +0900

    🐛 Fix : 포맷 형식 영어에서 한글로 만 가능하도록 변경

--- src/utils/categoryFormatting.ts
+++ src/utils/categoryFormatting.ts
@@ -1,31 +1,9 @@
 export const categoryFormatting = (category: string) => {
   switch (category) {
-    case '어학':
-      return 'LANGUAGE';
-    case 'IT':
-      return 'IT';
-    case '고시/자격증':
-      return 'EXAM';
-    case '금융':
-      return 'FINANCE';
-    case '경영':
-      return 'MANAGEMENT';
-    case '디자인':
-      return 'DESIGN';
-    case '예술':
-      return 'ART';
-    case '사진/영상':
-      return 'PHOTO_VIDEO';
-    case '뷰티':
-      return 'BEAUTY';
-    case '스포츠':
-      return 'SPORTS';
-    case '취미':
-      return 'HOBBY';
-    case '기타':
-      return 'ETC';
     case 'LANGUAGE':
       return '어학';
+    case 'IT':
+      return 'IT';
     case 'EXAM':
       return '고시/자격증';
     case 'FINANCE':
--- src/utils/studyTypeFormatting.ts
+++ src/utils/studyTypeFormatting.ts
@@ -1,11 +1,5 @@
 export const studyTypeFormatting = (type: string) => {
   switch (type) {
-    case '온라인':
-      return 'ONLINE';
-    case '오프라인':
-      return 'OFFLINE';
-    case '온/오프라인':
-      return 'HYBRID';
     case 'ONLINE':
       return '온라인';
     case 'OFFLINE':
