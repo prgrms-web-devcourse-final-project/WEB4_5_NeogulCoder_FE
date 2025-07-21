2bc386338b88daa619efb096eb9327f3fea423e4
sduddla <sduddla921@gmail.com>
2025-07-22 04:03:34 +0900

    ✨ Feat: PR 섹션 구현

--- src/components/profile/pr/FeedbackSection.tsx
+++ src/components/profile/pr/FeedbackSection.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
-import musicBunny from '@/assets/images/music-bunny.svg';
+// import musicBunny from '@/assets/images/music-bunny.svg';
 import { useEffect, useState } from 'react';
 import FeedbackModal from './FeedbackModal';
+import { userPrStore } from '@/stores/prStore';
 
 export default function FeedbackSection() {
-  const userName = '박스영';
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
@@ -19,42 +19,60 @@ export default function FeedbackSection() {
     };
   }, [isModalOpen]);
 
+  const { pr } = userPrStore();
+  if (!pr) return null;
+
   return (
     <>
-      <div className='w-1/2 h-[260px] border border-main/10 rounded-[10px] p-5'>
-        <div className='flex justify-between items-center mb-10'>
+      <div className='w-1/2 h-[260px] border border-main/10 rounded-[10px] p-5 flex flex-col'>
+        <div className='flex justify-between items-center'>
           <p className='tm3'>피드백</p>
-          <button
-            type='button'
-            className='text-text1/50 cursor-pointer t5'
-            onClick={() => setIsModalOpen(true)}
-          >
-            더보기
-          </button>
+          {pr.reviewContents.length > 0 && (
+            <button
+              type='button'
+              className='text-text1/50 cursor-pointer t5'
+              onClick={() => setIsModalOpen(true)}
+            >
+              더보기
+            </button>
+          )}
         </div>
 
-        <div className='flex flex-col gap-3'>
-          <div className='flex gap-3'>
-            <div className='w-[50px] h-[50px] bg-gray3 rounded-full overflow-hidden flex-shrink-0'>
-              <Image src={musicBunny} alt='임시 기본 프사' />
-            </div>
-
-            <div className='flex flex-col justify-center'>
-              <p className='tm4'>{userName}</p>
-              <p className='t5 text-text1/50'>2025.07.07</p>
-            </div>
+        {pr.reviewContents.length === 0 ? (
+          <div className='flex flex-1 justify-center items-center text-text1/50 t4'>
+            받은 피드백이 없습니다.
           </div>
+        ) : (
+          <div className='flex flex-col gap-3'>
+            <div className='flex gap-3'>
+              <div className='w-[50px] h-[50px] bg-gray3 rounded-full overflow-hidden flex-shrink-0'>
+                <Image
+                  src={pr.reviewContents[0]?.reviewUserImgUrl}
+                  alt='유저 프로필'
+                  width={50}
+                  height={50}
+                />
+              </div>
 
-          <p className='t4 text-text1 pl-[62px]'>
-            스터디 분위기메이커 스터디 분위기메이커 스터디 분위기메이커 스터디
-            분위기메이커
-          </p>
-        </div>
+              <div className='flex flex-col justify-center'>
+                <p className='tm4'>
+                  {pr.reviewContents[0]?.reviewUserNickname}
+                </p>
+                <p className='t5 text-text1/50'>
+                  {pr.reviewContents[0]?.reviewDate}
+                </p>
+              </div>
+            </div>
+
+            <p className='t4 text-text1 pl-[62px]'>
+              {pr.reviewContents[0]?.reviewComment}
+            </p>
+          </div>
+        )}
 
         {isModalOpen && (
           <div className='fixed inset-0 z-50 flex items-center justify-center'>
             <div className='absolute inset-0 bg-main opacity-80' />
-
             <div className='relative z-10'>
               <FeedbackModal onClose={() => setIsModalOpen(false)} />
             </div>
--- src/components/profile/pr/InfoSection.tsx
+++ src/components/profile/pr/InfoSection.tsx
@@ -1,50 +1,66 @@
-import { Link } from 'lucide-react';
+import { userPrStore } from '@/stores/prStore';
 
 export default function InfoSection() {
-  const userInfo = {
-    region: '서울시',
-    url: [
-      { label: '수코딩', url: 'https://www.sucoding.kr/', icon: 'sucoding' },
-      {
-        label: '수튜브',
-        url: 'https://www.youtube.com/@sucoding',
-        icon: 'syoutube',
-      },
-    ],
-  };
+  const { pr } = userPrStore();
+  if (!pr) return null;
+
+  const location = pr.userLocationAndLinks?.[0]?.location;
+  const links = pr.userLocationAndLinks?.[0]?.links;
+
+  const hasLocation = !!location;
+  const hasLinks = !!links && links.length > 0;
+
+  const locationLinksMessage = !hasLocation && !hasLinks;
+
   return (
     <>
       <div className='w-1/2 h-[180px] border border-main/10 rounded-[10px] p-5 flex flex-col'>
         <p className='tm3 mb-6'>정보</p>
 
-        <div className='flex items-center gap-10 mb-[18px]'>
-          <p className='t3 text-text1/50'>지역</p>
-          <span className='t3 text-text1'>{userInfo.region}</span>
-        </div>
-
-        <div className='flex gap-10'>
-          <p className='t3 text-text1/50'>URL</p>
-          <div className='flex flex-col gap-[10px]'>
-            {userInfo.url.map((item: any, id: number) => (
-              <div key={id} className='flex items-center gap-[10px]'>
-                {item.icon === 'sucoding' && (
-                  <Link className='w-4 h-4 text-text1' />
-                )}
-                {item.icon === 'syoutube' && (
-                  <Link className='w-4 h-4 text-text1' />
-                )}
-
-                <a
-                  href={item.url}
-                  target='_blank'
-                  className='t4 text-text1/50 underline hover:text-main transition'
-                >
-                  {item.label}
-                </a>
+        {locationLinksMessage ? (
+          <span className='text-text1/50 t4'>지역과 URL을 등록해 주세요</span>
+        ) : (
+          <>
+            {/* 지역 */}
+            <div className='flex gap-10'>
+              <div className='min-w-[50px]'>
+                <p className='t3 text-text1/50'>지역</p>
               </div>
-            ))}
-          </div>
-        </div>
+              <div className='flex items-center justify-center'>
+                {hasLocation ? (
+                  <span className='t4 text-text1'>{location}</span>
+                ) : (
+                  <span className='t4 text-text1/30'>지역을 등록해 주세요</span>
+                )}
+              </div>
+            </div>
+
+            {/* URL */}
+            <div className='flex gap-10 mt-[18px]'>
+              <div className='min-w-[50px]'>
+                <p className='t3 text-text1/50'>URL</p>
+              </div>
+              <div className='flex items-center justify-center'>
+                {hasLinks ? (
+                  <div className='flex flex-col gap-[10px]'>
+                    {links.map((linkItem, index) => (
+                      <a
+                        key={index}
+                        href={linkItem.link}
+                        target='_blank'
+                        className='t4 text-text1 underline'
+                      >
+                        {linkItem.linkName}
+                      </a>
+                    ))}
+                  </div>
+                ) : (
+                  <span className='t4 text-text1/30'>URL을 등록해 주세요</span>
+                )}
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
