52591d616b5e227688e85ce3c1993414a4973aab
yubin121 <yubin045@naver.com>
2025-07-24 00:35:54 +0900

    ✨ Feat: My 정보 페이지 api 추가

--- src/lib/api/community.ts
+++ src/lib/api/community.ts
@@ -1,13 +1,87 @@
 import axiosInstance from '@/lib/api/axiosInstance';
-import { RequestBodyType } from '@/types/community';
+import { MyListQueryType, StudyListQueryType } from '@/types/community';
 
 export const fetchStudyCommunityList = async (
   studyId: number,
-  body: RequestBodyType
+  params: StudyListQueryType
 ) => {
-  const { data } = await axiosInstance.post(
-    `/api/studies/${studyId}/posts/search`,
-    body
+  const {
+    page,
+    pageSize = 10,
+    category,
+    content,
+    attributeName = 'createDateTime',
+    sort = 'DESC',
+  } = params;
+
+  const query = new URLSearchParams({
+    page: page.toString(),
+    pageSize: pageSize.toString(),
+    ...(category ? { category } : {}),
+    ...(content ? { content } : {}),
+    attributeName,
+    sort,
+  });
+
+  const { data } = await axiosInstance.get(
+    `/api/studies/${studyId}/posts/search?${query.toString()}`
   );
+  return data.data;
+};
+
+export const fetchMyCommunityList = async (
+  studyId: number,
+  params: MyListQueryType
+) => {
+  const {
+    page,
+    pageSize = 10,
+    category,
+    content,
+    attributeName = 'createDateTime',
+    sort = 'DESC',
+  } = params;
+
+  const query = new URLSearchParams({
+    page: page.toString(),
+    pageSize: pageSize.toString(),
+    ...(category ? { category } : {}),
+    ...(content ? { content } : {}),
+    attributeName,
+    sort,
+  });
+
+  const { data } = await axiosInstance.get(
+    `/api/studies/${studyId}/posts/search/me?${query.toString()}`
+  );
+  return data.data;
+};
+
+export const deleteMeByStudy = async (studyId: number) => {
+  const { data } = await axiosInstance.delete(`/api/studies/${studyId}/me`);
+  return data.data;
+};
+
+export const participateInStudyExtension = async (studyId: number) => {
+  const { data } = await axiosInstance.post(
+    `/api/studies/${studyId}/extension/participations`
+  );
+  return data.data;
+};
+
+export const checkStudyExtension = async (studyId: number) => {
+  const { data } = await axiosInstance.get(`/api/studies/${studyId}/extension`);
+  return data.data;
+};
+
+export const checkParticipationList = async (studyId: number) => {
+  const { data } = await axiosInstance.get(
+    `/api/studies/${studyId}/extension/participations`
+  );
+  return data.data;
+};
+
+export const checkMyRoleInStudy = async (studyId: number) => {
+  const { data } = await axiosInstance.get(`/api/studies/${studyId}/me`);
   return data.data;
 };
