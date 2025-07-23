492b7eaa76b672f3c4ca1a2375c11afe324c62a3
yubin121 <yubin045@naver.com>
2025-07-24 00:37:05 +0900

    ✨ Feat: CSR 구현

--- src/app/(common)/study/[id]/study-community/page.tsx
+++ src/app/(common)/study/[id]/study-community/page.tsx
@@ -1,52 +1,92 @@
-import StudyCommunityClient from '@/components/study/StudyCommunityClient';
+'use client';
 
+import StudyCommunityClient from '@/components/study/StudyCommunityClient';
+import { fetchStudyCommunityList } from '@/lib/api/community';
+import { useParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+// export default async function StudyCommunity({
+//   params,
+// }: {
+//   params: Promise<{ id: string }>;
+// }) {
 export default function StudyCommunity() {
-  const responseType = {
-    noticePostInfos: [
-      {
-        postId: 3,
-        category: '공지',
-        title: '제목',
-        createdAt: '2025-07-21',
-      },
-      {
-        postId: 4,
-        category: '공지',
-        title:
-          '공지인데 뭐 공지인데 뭐 공지인데 뭐 공지인데 뭐 공지인데 뭐 공지인데 뭐 공지인데 뭐 공지인데 뭐',
-        createdAt: '2025-07-22',
-      },
-    ],
-    postInfos: [
-      {
-        id: 12,
-        title: '모든 국민은 직업선택의 자유를 가진다.',
-        category: 'NOTICE',
-        content: '국회는 의원의 자격을 심사하며, 의원을 징계할 있다.',
-        createdDate: '2025-07-21T16:20:58.523Z',
-        commentCount: 3,
-      },
-      {
-        id: 13,
-        title:
-          '아라라라ㅏ라 나난ㄴ나나나ㅏㅏ난나ㅏㅏㄴ나 바나나나나나나미니언 미니언미니언',
-        category: 'FREE',
-        content:
-          '국회는 의원의 자격을 심사하며, 의원을 징계할 있다. 국회는 의원의 자격을 심사하며, 의원을 징계할 있다. 국회는 의원의 자격을 심사하며, 의원을 징계할 있다. 국회는 의원의 자격을 심사하며, 의원을 징계할 있다.',
-        createdDate: '2025-07-22T16:20:58.523Z',
-        commentCount: 0,
-      },
-    ],
-    totalPage: 13,
-    totalElementCount: 3,
-    hasNext: true,
+  // const { id } = await params;
+  const { id } = useParams();
+
+  const query = {
+    page: 0,
+    pageSize: 10,
+    attributeName: 'createDateTime',
+    sort: 'DESC',
+  };
+  // const data = await fetchStudyCommunityList(Number(id), query);
+
+  const [initialStudyCommunityData, setInitialStudyCommunityData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const init = async () => {
+    const data = await fetchStudyCommunityList(Number(id), query);
+    setInitialStudyCommunityData(data);
+
+    setIsLoading(false);
   };
 
-  const initialStudyCommunityData = responseType;
+  useEffect(() => {
+    init();
+  }, []);
+
+  // const responseType = {
+  //   noticePostInfos: [
+  //     {
+  //       postId: 3,
+  //       category: '공지',
+  //       title: '제목',
+  //       createdAt: '2025-07-21',
+  //     },
+  //     {
+  //       postId: 4,
+  //       category: '공지',
+  //       title:
+  //         '공지인데 뭐 공지인데 뭐 공지인데 뭐 공지인데 뭐 공지인데 뭐 공지인데 뭐 공지인데 뭐 공지인데 뭐',
+  //       createdAt: '2025-07-22',
+  //     },
+  //   ],
+  //   postInfos: [
+  //     {
+  //       id: 12,
+  //       title: '모든 국민은 직업선택의 자유를 가진다.',
+  //       category: 'NOTICE',
+  //       content: '국회는 의원의 자격을 심사하며, 의원을 징계할 있다.',
+  //       createdDate: '2025-07-21T16:20:58.523Z',
+  //       commentCount: 3,
+  //     },
+  //     {
+  //       id: 13,
+  //       title:
+  //         '아라라라ㅏ라 나난ㄴ나나나ㅏㅏ난나ㅏㅏㄴ나 바나나나나나나미니언 미니언미니언',
+  //       category: 'FREE',
+  //       content:
+  //         '국회는 의원의 자격을 심사하며, 의원을 징계할 있다. 국회는 의원의 자격을 심사하며, 의원을 징계할 있다. 국회는 의원의 자격을 심사하며, 의원을 징계할 있다. 국회는 의원의 자격을 심사하며, 의원을 징계할 있다.',
+  //       createdDate: '2025-07-22T16:20:58.523Z',
+  //       commentCount: 0,
+  //     },
+  //   ],
+  //   totalPage: 1,
+  //   totalElementCount: 2,
+  //   hasNext: false,
+  // };
+
+  // const initialStudyCommunityData = data || responseType;
+  console.log('initialStudyCommunityData:', initialStudyCommunityData);
+
+  if (isLoading) {
+    return <></>;
+  }
 
   return (
     <StudyCommunityClient
-      initialStudyCommunityData={initialStudyCommunityData}
+      initialStudyCommunityData={initialStudyCommunityData!}
     />
   );
 }
--- src/components/study/StudyCommunityClient.tsx
+++ src/components/study/StudyCommunityClient.tsx
@@ -5,7 +5,7 @@ import RecruitmentCard from '@/components/my/RecruitmentCard';
 import ListMenuStudy from '@/components/study/ListMenuStudy';
 import NoticeItem from '@/components/study/NoticeItem';
 import { fetchStudyCommunityList } from '@/lib/api/community';
-import { StudyCommunityData } from '@/types/community';
+import { StudyListType } from '@/types/community';
 import { useParams } from 'next/navigation';
 import { useCallback, useEffect, useState } from 'react';
 
@@ -23,7 +23,7 @@ const sortingMap: Record<string, 'createDateTime' | 'commentCount'> = {
 export default function StudyCommunityClient({
   initialStudyCommunityData,
 }: {
-  initialStudyCommunityData: StudyCommunityData;
+  initialStudyCommunityData: StudyListType;
 }) {
   const { id } = useParams();
   const studyId = Number(id);
@@ -33,7 +33,7 @@ export default function StudyCommunityClient({
   const [keyword, setKeyword] = useState('');
 
   const [studyCommunityList, setStudyCommunityList] = useState<
-    StudyCommunityData['postInfos']
+    StudyListType['postInfos']
   >(initialStudyCommunityData.postInfos);
 
   const [page, setPage] = useState(0);
@@ -42,15 +42,19 @@ export default function StudyCommunityClient({
   );
 
   const filterList = useCallback(async () => {
-    const body = {
+    const categoryValue = categoryMap[selectedCategory];
+    const sortingValue = sortingMap[selectedSortingType];
+
+    const query = {
       page,
       pageSize: 10,
-      category: categoryMap[selectedCategory],
-      content: keyword,
-      attributeName: sortingMap[selectedSortingType],
+      attributeName: sortingValue,
       sort: 'DESC',
+      ...(categoryValue ? { category: categoryValue } : {}),
+      ...(keyword ? { content: keyword } : {}),
     };
-    const data = await fetchStudyCommunityList(studyId, body);
+
+    const data = await fetchStudyCommunityList(studyId, query);
     setStudyCommunityList(data.postInfos);
     setTotalPages(data.totalPage);
   }, [selectedCategory, selectedSortingType, keyword, page, studyId]);
@@ -73,14 +77,28 @@ export default function StudyCommunityClient({
         />
       </div>
       <div className='flex flex-col gap-4 p-6 bg-[#fafafa] rounded-[8px] mt-[30px]'>
-        {initialStudyCommunityData.noticePostInfos.map((notice) => (
-          <NoticeItem key={notice.postId} {...notice} />
-        ))}
+        {initialStudyCommunityData.noticePostInfos.length === 0 && (
+          <div className='text-center tm3 text-text1/80'>공지가 없습니다.</div>
+        )}
+        {initialStudyCommunityData.noticePostInfos.length !== 0 &&
+          initialStudyCommunityData.noticePostInfos.map((notice) => (
+            <NoticeItem key={notice.postId} {...notice} />
+          ))}
       </div>
-      <div className='flex flex-col gap-[30px] mt-[30px]'>
-        {studyCommunityList.map((post) => (
-          <RecruitmentCard key={post.id} type='study' {...post} />
-        ))}
+      <div
+        className={`flex flex-col gap-[30px] mt-[30px] relative ${
+          studyCommunityList.length === 0 && 'h-[calc(100vh-105px-113px-198px)]'
+        }`}
+      >
+        {studyCommunityList.length === 0 && (
+          <div className='flex justify-center items-center absolute top-[45%] left-1/2 -translate-x-1/2 -translate-y-1/2 tm2 text-text1/80'>
+            게시글이 없습니다.
+          </div>
+        )}
+        {studyCommunityList.length !== 0 &&
+          studyCommunityList.map((post) => (
+            <RecruitmentCard key={post.id} type='study' {...post} />
+          ))}
       </div>
       <div className='mt-[45px]'>
         <Pagination
