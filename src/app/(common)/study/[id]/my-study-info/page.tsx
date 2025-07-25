ea06665a00059dd71817685759f9fc06ad163919
yubin121 <yubin045@naver.com>
2025-07-26 01:45:27 +0900

    🔥 Remove: 불필요한 주석 제거

--- src/app/(common)/study/[id]/my-study-info/page.tsx
+++ src/app/(common)/study/[id]/my-study-info/page.tsx
@@ -52,32 +52,6 @@ export default function MyStudyInfo() {
     init();
   }, []);
 
-  // const responseType = {
-  //   postInfos: [
-  //     {
-  //       id: 12,
-  //       title: '모든 국민은 직업선택의 자유를 가진다.',
-  //       category: 'NOTICE',
-  //       content: '국회는 의원의 자격을 심사하며, 의원을 징계할 있다.',
-  //       createdDate: '2025-07-21T16:20:58.523Z',
-  //       commentCount: 3,
-  //     },
-  //     {
-  //       id: 13,
-  //       title:
-  //         '아라라라ㅏ라 나난ㄴ나나나ㅏㅏ난나ㅏㅏㄴ나 바나나나나나나미니언 미니언미니언',
-  //       category: 'FREE',
-  //       content:
-  //         '국회는 의원의 자격을 심사하며, 의원을 징계할 있다. 국회는 의원의 자격을 심사하며, 의원을 징계할 있다. 국회는 의원의 자격을 심사하며, 의원을 징계할 있다. 국회는 의원의 자격을 심사하며, 의원을 징계할 있다.',
-  //       createdDate: '2025-07-22T16:20:58.523Z',
-  //       commentCount: 0,
-  //     },
-  //   ],
-  //   totalPage: 1,
-  //   totalElementCount: 2,
-  //   hasNext: false,
-  // };
-
   // const initialMyCommunityData = communityListData || responseType;
   console.log('initialMyCommunityData:', initialMyCommunityData);
   console.log('extensionData:', extensionData);
--- src/app/(common)/study/[id]/study-community/page.tsx
+++ src/app/(common)/study/[id]/study-community/page.tsx
@@ -36,47 +36,6 @@ export default function StudyCommunity() {
     init();
   }, []);
 
-  // const responseType = {
-  //   noticePostInfos: [
-  //     {
-  //       postId: 3,
-  //       category: '공지',
-  //       title: '제목',
-  //       createdAt: '2025-07-21',
-  //     },
-  //     {
-  //       postId: 4,
-  //       category: '공지',
-  //       title:
-  //         '공지인데 뭐 공지인데 뭐 공지인데 뭐 공지인데 뭐 공지인데 뭐 공지인데 뭐 공지인데 뭐 공지인데 뭐',
-  //       createdAt: '2025-07-22',
-  //     },
-  //   ],
-  //   postInfos: [
-  //     {
-  //       id: 12,
-  //       title: '모든 국민은 직업선택의 자유를 가진다.',
-  //       category: 'NOTICE',
-  //       content: '국회는 의원의 자격을 심사하며, 의원을 징계할 있다.',
-  //       createdDate: '2025-07-21T16:20:58.523Z',
-  //       commentCount: 3,
-  //     },
-  //     {
-  //       id: 13,
-  //       title:
-  //         '아라라라ㅏ라 나난ㄴ나나나ㅏㅏ난나ㅏㅏㄴ나 바나나나나나나미니언 미니언미니언',
-  //       category: 'FREE',
-  //       content:
-  //         '국회는 의원의 자격을 심사하며, 의원을 징계할 있다. 국회는 의원의 자격을 심사하며, 의원을 징계할 있다. 국회는 의원의 자격을 심사하며, 의원을 징계할 있다. 국회는 의원의 자격을 심사하며, 의원을 징계할 있다.',
-  //       createdDate: '2025-07-22T16:20:58.523Z',
-  //       commentCount: 0,
-  //     },
-  //   ],
-  //   totalPage: 1,
-  //   totalElementCount: 2,
-  //   hasNext: false,
-  // };
-
   // const initialStudyCommunityData = data || responseType;
   console.log('initialStudyCommunityData:', initialStudyCommunityData);
 
