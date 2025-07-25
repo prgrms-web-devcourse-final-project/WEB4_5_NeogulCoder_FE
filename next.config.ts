b81f9c20071fecb55d4edd0333d1fc93d45ed6ec
sduddla <sduddla921@gmail.com>
2025-07-25 16:57:45 +0900

    ✨ Feat: 외부 이미지 등록 설정

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,29 @@
-import type { NextConfig } from 'next';
+// import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
   images: {
-    // 팀채팅 임시 프로필 사진 때문에 넣은거. 추후에 지워도 됨
     remotePatterns: [
-      new URL('https://i.pinimg.com/**'),
-      new URL('https://dummyimage.com/**'),
-      new URL('http://localhost:8083/**'),
-      new URL('https://wibby.com/profile/**'),
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        pathname: '**', // 모든 경로 허용
+      },
     ],
   },
 };
 
-export default nextConfig;
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   images: {
+//     // 팀채팅 임시 프로필 사진 때문에 넣은거. 추후에 지워도 됨
+//     remotePatterns: [
+//       new URL('https://i.pinimg.com/**'),
+//       new URL('https://dummyimage.com/**'),
+//       new URL('http://localhost:8083/**'),
+//       new URL('https://wibby.com/profile/**'),
+
+//     ],
+//   },
+// };
+
+// export default nextConfig;
