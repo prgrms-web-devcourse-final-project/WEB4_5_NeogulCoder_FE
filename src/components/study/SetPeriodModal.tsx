4c10362786639a8fd69360dfc3bd88fea202b1e1
yubin121 <yubin045@naver.com>
2025-07-26 01:26:55 +0900

    ✨ Feat: 기간 설정 모달 구현

--- src/components/study/SetPeriodModal.tsx
+++ src/components/study/SetPeriodModal.tsx
@@ -1,23 +1,45 @@
 'use client';
 
+import { setTimeVotePeriods } from '@/lib/api/schedule';
+import { formatDate } from '@/utils/formatDate';
+import dayjs from 'dayjs';
 import { CalendarDays, X } from 'lucide-react';
+import { useState } from 'react';
 
 export default function SetPeriodModal({
-  closeHandler,
+  studyId,
+  onClose,
 }: {
-  closeHandler: () => void;
+  studyId: number;
+  onClose: () => void;
 }) {
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+
+  const handleClick = async () => {
+    try {
+      const start = formatDate(startDate, 'YYYY-MM-DDT00:00:00');
+      const end = formatDate(endDate, 'YYYY-MM-DDT23:59:59');
+      await setTimeVotePeriods(studyId, start, end);
+      onClose();
+      window.location.reload();
+      // toast message
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <>
       <div className='bg-black/50 fixed top-0 bottom-0 left-0 right-0 z-15 flex items-center justify-center'>
         <div className='w-[580px] flex flex-col gap-12 px-9 py-7 rounded-[10px] bg-white drop-shadow-md'>
           <div className='flex justify-between'>
             <span className='tm2'>기간 설정</span>
-            <button onClick={closeHandler}>
+            <button onClick={onClose}>
               <X className='w-8 y-8' />
             </button>
           </div>
-          <form>
+          <div>
             <div className='shrink-0'>
               <p className='t3'>
                 기간 <span className='tm5 text-red'>(필수)</span>
@@ -26,7 +48,14 @@ export default function SetPeriodModal({
                 <label className='w-full relative'>
                   <input
                     type='date'
+                    value={startDate}
                     className='date-custom w-full input-type2 tm3 pr-9!'
+                    onChange={(e) => setStartDate(e.target.value)}
+                    min={
+                      endDate &&
+                      dayjs(endDate).subtract(6, 'day').format('YYYY-MM-DD')
+                    }
+                    max={endDate && dayjs(endDate).format('YYYY-MM-DD')}
                   />
                   <CalendarDays
                     strokeWidth={1}
@@ -37,7 +66,14 @@ export default function SetPeriodModal({
                 <label className='w-full relative'>
                   <input
                     type='date'
+                    value={endDate}
                     className='date-custom w-full input-type2 tm3 pr-9!'
+                    onChange={(e) => setEndDate(e.target.value)}
+                    min={startDate && dayjs(startDate).format('YYYY-MM-DD')}
+                    max={
+                      startDate &&
+                      dayjs(startDate).add(6, 'day').format('YYYY-MM-DD')
+                    }
                   />
                   <CalendarDays
                     strokeWidth={1}
@@ -50,9 +86,11 @@ export default function SetPeriodModal({
               </span>
             </div>
             <div className='mt-12'>
-              <button className='button-modal1'>요청</button>
+              <button className='button-modal1' onClick={handleClick}>
+                요청
+              </button>
             </div>
-          </form>
+          </div>
         </div>
       </div>
     </>
