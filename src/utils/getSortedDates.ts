c96eec77db775ee4a359cc9e1b6374a9a72388d1
yubin121 <yubin045@naver.com>
2025-07-26 01:26:26 +0900

    ✨ Feat: 요일 순서에 맞게 날짜 조정 및 포맷팅 하는 유틸 함수 구현

---
+++ src/utils/getSortedDates.ts
@@ -0,0 +1,46 @@
+import dayjs from 'dayjs';
+
+type WeekData = {
+  weekdays: string[];
+  dates: string[];
+};
+
+export function getSortedDates(
+  startDateStr: string,
+  endDateStr: string
+): WeekData {
+  const start = dayjs(startDateStr);
+  const end = dayjs(endDateStr);
+
+  const weekdays = ['일', '월', '화', '수', '목', '금', '토'];
+  const weekdayToDate = new Map<number, string>();
+
+  let cursor = start;
+  while (cursor.isBefore(end) || cursor.isSame(end, 'day')) {
+    const dayIndex = cursor.day();
+    if (!weekdayToDate.has(dayIndex)) {
+      weekdayToDate.set(dayIndex, cursor.format('MM.DD'));
+    }
+    cursor = cursor.add(1, 'day');
+  }
+
+  for (let dayIndex = 0; dayIndex < 7; dayIndex++) {
+    if (!weekdayToDate.has(dayIndex)) {
+      let backCursor = start.subtract(1, 'day');
+      while (true) {
+        if (backCursor.day() === dayIndex) {
+          weekdayToDate.set(dayIndex, `n${backCursor.format('MM.DD')}`);
+          break;
+        }
+        backCursor = backCursor.subtract(1, 'day');
+      }
+    }
+  }
+
+  const dates = weekdays.map((_, index) => weekdayToDate.get(index)!);
+
+  return {
+    weekdays,
+    dates,
+  };
+}
