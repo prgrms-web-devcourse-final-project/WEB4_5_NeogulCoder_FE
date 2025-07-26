77602f03dc077783a81295bbe9ee8cd55ee4c63a
kang-min-ryu <dbrkdalswkd@gmail.com>
2025-07-26 15:33:29 +0900

    ✨ Feat: 스터디 커뮤니티 이미지 업로드 기능 추가

--- src/components/common/Editor.tsx
+++ src/components/common/Editor.tsx
@@ -1,26 +1,51 @@
+'use client';
+
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor as ToastEditor } from '@toast-ui/react-editor';
 import { useEffect } from 'react';
+import { axiosInstance } from '../../lib/api/axios';
 
 type Props = {
-  editorRef: React.RefObject<ToastEditor | null>;
+  editorRef: React.RefObject<ToastEditor>;
   content?: string;
 };
 
 export default function MyEditor({ editorRef, content }: Props) {
+  const handleImageUpload = async (
+    blob: Blob,
+    callback: (url: string, alt?: string) => void
+  ) => {
+    try {
+      const formData = new FormData();
+      formData.append('file', blob);
+
+      const response = await axiosInstance.post(
+        '/api/studies/posts/images',
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        }
+      );
+
+      const imageUrl = response?.data?.message;
+      if (!imageUrl) {
+        throw new Error('이미지 URL 없음');
+      }
+
+      callback(imageUrl, 'uploaded image');
+    } catch (error) {
+      console.error('이미지 업로드 실패:', error);
+    }
+  };
+
   useEffect(() => {
     const instance = editorRef.current?.getInstance();
-    if (instance) {
+    if (instance && content) {
       instance.setMarkdown(content);
     }
   }, [editorRef, content]);
-
-  // const handleChange = () => {
-  //   const markdown = editorRef.current?.getInstance().getMarkdown();
-  //   if (markdown !== undefined) {
-  //     onChange(markdown);
-  //   }
-  // };
 
   return (
     <div className='tm3'>
@@ -31,8 +56,10 @@ export default function MyEditor({ editorRef, content }: Props) {
         initialEditType='wysiwyg'
         useCommandShortcut={true}
         hideModeSwitch={true}
+        hooks={{
+          addImageBlobHook: handleImageUpload,
+        }}
         initialValue=''
-        // onChange={handleChange}
       />
     </div>
   );
