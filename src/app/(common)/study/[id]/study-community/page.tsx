d61f1fc5dc9bc5e5b49f02967ad235ea07daf867
yubin121 <yubin045@naver.com>
2025-07-27 19:45:13 +0900

    🐛 Fix: 데이터 페칭 제거

--- src/app/(common)/study/[id]/study-community/page.tsx
+++ src/app/(common)/study/[id]/study-community/page.tsx
@@ -1,9 +1,4 @@
-'use client';
-
 import StudyCommunityClient from '@/components/study/StudyCommunityClient';
-import { fetchStudyCommunityList } from '@/lib/api/community';
-import { useParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
 
 // export default async function StudyCommunity({
 //   params,
@@ -12,40 +7,14 @@ import { useEffect, useState } from 'react';
 // }) {
 export default function StudyCommunity() {
   // const { id } = await params;
-  const { id } = useParams();
 
-  const query = {
-    page: 0,
-    pageSize: 10,
-    attributeName: 'createDateTime',
-    sort: 'DESC',
-  };
+  // const query = {
+  //   page: 0,
+  //   pageSize: 10,
+  //   attributeName: 'createDateTime',
+  //   sort: 'DESC',
+  // };
   // const data = await fetchStudyCommunityList(Number(id), query);
 
-  const [initialStudyCommunityData, setInitialStudyCommunityData] = useState();
-  const [isLoading, setIsLoading] = useState(true);
-
-  const init = async () => {
-    const data = await fetchStudyCommunityList(Number(id), query);
-    setInitialStudyCommunityData(data);
-
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    init();
-  }, []);
-
-  // const initialStudyCommunityData = data || responseType;
-  console.log('initialStudyCommunityData:', initialStudyCommunityData);
-
-  if (isLoading) {
-    return <></>;
-  }
-
-  return (
-    <StudyCommunityClient
-      initialStudyCommunityData={initialStudyCommunityData!}
-    />
-  );
+  return <StudyCommunityClient />;
 }
