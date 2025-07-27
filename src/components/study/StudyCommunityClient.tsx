ba3246162c968813d4aa7f13fc1d75792172c6c9
yubin121 <yubin045@naver.com>
2025-07-27 19:45:32 +0900

    ✨ Feat: 스켈레톤 적용

--- src/components/study/StudyCommunityClient.tsx
+++ src/components/study/StudyCommunityClient.tsx
@@ -8,6 +8,8 @@ import { fetchStudyCommunityList } from '@/lib/api/community';
 import { StudyListType } from '@/types/community';
 import { useParams } from 'next/navigation';
 import { useCallback, useEffect, useState } from 'react';
+import NoticeItemSkeleton from './NoticeItemSkeleton';
+import CommunityCardSkeleton from './CommunityCardSkeleton';
 
 const categoryMap: Record<string, '' | 'NOTICE' | 'FREE'> = {
   전체: '',
@@ -20,11 +22,7 @@ const sortingMap: Record<string, 'createDateTime' | 'commentCount'> = {
   댓글순: 'commentCount',
 };
 
-export default function StudyCommunityClient({
-  initialStudyCommunityData,
-}: {
-  initialStudyCommunityData: StudyListType;
-}) {
+export default function StudyCommunityClient() {
   const { id } = useParams();
   const studyId = Number(id);
 
@@ -32,31 +30,41 @@ export default function StudyCommunityClient({
   const [selectedSortingType, setSelectedSortingType] = useState('최신순');
   const [keyword, setKeyword] = useState('');
 
+  const [studyNoticeList, setStudyNoticeList] = useState<
+    StudyListType['noticePostInfos']
+  >([]);
   const [studyCommunityList, setStudyCommunityList] = useState<
     StudyListType['postInfos']
-  >(initialStudyCommunityData.postInfos);
+  >([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const [page, setPage] = useState(0);
-  const [totalPages, setTotalPages] = useState(
-    initialStudyCommunityData.totalPage
-  );
+  const [totalPages, setTotalPages] = useState(0);
 
   const filterList = useCallback(async () => {
-    const categoryValue = categoryMap[selectedCategory];
-    const sortingValue = sortingMap[selectedSortingType];
+    setIsLoading(true);
+    try {
+      const categoryValue = categoryMap[selectedCategory];
+      const sortingValue = sortingMap[selectedSortingType];
 
-    const query = {
-      page,
-      pageSize: 10,
-      attributeName: sortingValue,
-      sort: 'DESC',
-      ...(categoryValue ? { category: categoryValue } : {}),
-      ...(keyword ? { keyword } : {}),
-    };
+      const query = {
+        page,
+        pageSize: 10,
+        attributeName: sortingValue,
+        sort: 'DESC',
+        ...(categoryValue ? { category: categoryValue } : {}),
+        ...(keyword ? { keyword } : {}),
+      };
 
-    const data = await fetchStudyCommunityList(studyId, query);
-    setStudyCommunityList(data.postInfos);
-    setTotalPages(data.totalPage);
+      const data = await fetchStudyCommunityList(studyId, query);
+      setStudyNoticeList(data.noticePostInfos);
+      setStudyCommunityList(data.postInfos);
+      setTotalPages(data.totalPage);
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setIsLoading(false);
+    }
   }, [selectedCategory, selectedSortingType, keyword, page, studyId]);
 
   useEffect(() => {
@@ -77,28 +85,50 @@ export default function StudyCommunityClient({
         />
       </div>
       <div className='flex flex-col gap-4 p-6 bg-[#fafafa] rounded-[8px] mt-[30px]'>
-        {initialStudyCommunityData.noticePostInfos.length === 0 && (
-          <div className='text-center tm3 text-text1/80'>공지가 없습니다.</div>
+        {isLoading ? (
+          <>
+            {Array.from({ length: 2 }).map((_, i) => (
+              <NoticeItemSkeleton key={i} />
+            ))}
+          </>
+        ) : (
+          <>
+            {studyNoticeList.length === 0 && (
+              <div className='text-center tm3 text-text1/80'>
+                공지가 없습니다.
+              </div>
+            )}
+            {studyNoticeList.length !== 0 &&
+              studyNoticeList.map((notice) => (
+                <NoticeItem key={notice.postId} {...notice} />
+              ))}
+          </>
         )}
-        {initialStudyCommunityData.noticePostInfos.length !== 0 &&
-          initialStudyCommunityData.noticePostInfos.map((notice) => (
-            <NoticeItem key={notice.postId} {...notice} />
-          ))}
       </div>
       <div
         className={`flex flex-col gap-[30px] mt-[30px] relative ${
           studyCommunityList.length === 0 && 'h-[calc(100vh-105px-113px-198px)]'
         }`}
       >
-        {studyCommunityList.length === 0 && (
-          <div className='flex justify-center items-center absolute top-[45%] left-1/2 -translate-x-1/2 -translate-y-1/2 tm2 text-text1/80'>
-            게시글이 없습니다.
-          </div>
+        {isLoading ? (
+          <>
+            {Array.from({ length: 3 }).map((_, i) => (
+              <CommunityCardSkeleton key={i} />
+            ))}
+          </>
+        ) : (
+          <>
+            {studyCommunityList.length === 0 && (
+              <div className='flex justify-center items-center absolute top-[45%] left-1/2 -translate-x-1/2 -translate-y-1/2 tm2 text-text1/80'>
+                게시글이 없습니다.
+              </div>
+            )}
+            {studyCommunityList.length !== 0 &&
+              studyCommunityList.map((post) => (
+                <RecruitmentCard key={post.id} type='study' {...post} />
+              ))}
+          </>
         )}
-        {studyCommunityList.length !== 0 &&
-          studyCommunityList.map((post) => (
-            <RecruitmentCard key={post.id} type='study' {...post} />
-          ))}
       </div>
       <div className='mt-[45px]'>
         <Pagination
