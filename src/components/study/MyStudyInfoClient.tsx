01939041928fc3577d58bfc67cc68b138c937db4
yubin121 <yubin045@naver.com>
2025-07-28 09:54:52 +0900

    ✨ Feat: 스켈레톤 적용

--- src/components/study/MyStudyInfoClient.tsx
+++ src/components/study/MyStudyInfoClient.tsx
@@ -4,16 +4,17 @@ import Pagination from '@/components/common/Pagination';
 import RecruitmentCard from '@/components/my/RecruitmentCard';
 import ListMenuStudy from '@/components/study/ListMenuStudy';
 import StudyOutCheckModal from '@/components/study-room/dashboard/StudyOutCheckModal';
-import {
-  ExtensionType,
-  MyInfoInStudyType,
-  MyListType,
-} from '@/types/community';
+import { ExtensionType, MyListType } from '@/types/community';
 import { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
-import { fetchMyCommunityList } from '@/lib/api/community';
+import {
+  checkStudyExtension,
+  checkMyRoleInStudy,
+  fetchMyCommunityList,
+} from '@/lib/api/community';
 import StudyExtendCheckModal from '@/components/study-room/dashboard/StudyExtendCheckModal';
 import { userAuthStore } from '@/stores/userStore';
+import MyStudyInfoSkeleton from './MyStudyInfoSkeleton';
 
 const categoryMap: Record<string, '' | 'NOTICE' | 'FREE'> = {
   전체: '',
@@ -26,23 +27,7 @@ const sortingMap: Record<string, 'createDateTime' | 'commentCount'> = {
   댓글순: 'commentCount',
 };
 
-export default function MyStudyInfoClient({
-  initialMyCommunityData,
-  extensionData,
-  // isExtended,
-  // isLeader,
-  myData,
-}: {
-  initialMyCommunityData: MyListType;
-  extensionData: ExtensionType;
-  // isExtended: boolean;
-  // isLeader: boolean;
-  myData: MyInfoInStudyType;
-}) {
-  // 임시 CSR용
-  const isExtended = extensionData.extended;
-  const isLeader = myData.role === 'LEADER';
-
+export default function MyStudyInfoClient() {
   const { id } = useParams();
   const studyId = Number(id);
 
@@ -56,12 +41,15 @@ export default function MyStudyInfoClient({
 
   const [myCommunityList, setMyCommunityList] = useState<
     MyListType['postInfos']
-  >(initialMyCommunityData.postInfos);
+  >([]);
+  // const [extensionData, setExtensionData] = useState();
+  // const [myData, setMyData] = useState();
+  const [isExtended, setIsExtended] = useState(false);
+  const [isLeader, setIsLeader] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const [page, setPage] = useState(0);
-  const [totalPages, setTotalPages] = useState(
-    initialMyCommunityData.totalPage
-  );
+  const [totalPages, setTotalPages] = useState(0);
 
   const [isOpenOutModal, setIsOpenOutModal] = useState(false);
   const [isOpenExtendModal, setIsOpenExtendModal] = useState(false);
@@ -73,158 +61,205 @@ export default function MyStudyInfoClient({
     setIsOpenExtendModal(false);
   };
 
-  const checkMyParticipation = useCallback(() => {
-    extensionData.members.forEach((member) => {
-      if (member.userId === me?.id) setIsParticipated(member.participated);
-    });
-  }, [extensionData.members, me?.id]);
+  const checkMyParticipation = useCallback(
+    (extend: ExtensionType['members']) => {
+      extend.forEach((member) => {
+        if (member.userId === me?.id) setIsParticipated(member.participated);
+      });
+    },
+    [me?.id]
+  );
+
+  const checkExtension = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const extendData = await checkStudyExtension(studyId);
+      // setExtensionData(extendData);
+      setIsExtended(extendData.extended);
+      checkMyParticipation(extendData.members);
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [checkMyParticipation, studyId]);
+
+  const checkMyRole = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const roleData = await checkMyRoleInStudy(studyId);
+      // setMyData(roleData);
+      setIsLeader(roleData.role === 'LEADER');
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [studyId]);
 
   const filterList = useCallback(async () => {
-    const categoryValue = categoryMap[selectedCategory];
-    const sortingValue = sortingMap[selectedSortingType];
+    setIsLoading(true);
+    try {
+      const categoryValue = categoryMap[selectedCategory];
+      const sortingValue = sortingMap[selectedSortingType];
 
-    const query = {
-      page,
-      pageSize: 5,
-      attributeName: sortingValue,
-      sort: 'DESC',
-      ...(categoryValue ? { category: categoryValue } : {}),
-      ...(keyword ? { keyword } : {}),
-    };
+      const query = {
+        page,
+        pageSize: 5,
+        attributeName: sortingValue,
+        sort: 'DESC',
+        ...(categoryValue ? { category: categoryValue } : {}),
+        ...(keyword ? { keyword } : {}),
+      };
 
-    const data = await fetchMyCommunityList(studyId, query);
-    setMyCommunityList(data.postInfos);
-    setTotalPages(data.totalPage);
+      const data = await fetchMyCommunityList(studyId, query);
+      setMyCommunityList(data.postInfos);
+      setTotalPages(data.totalPage);
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setIsLoading(false);
+    }
   }, [selectedCategory, selectedSortingType, keyword, page, studyId]);
+
+  useEffect(() => {
+    checkExtension();
+    checkMyRole();
+  }, [checkExtension, checkMyRole]);
 
   useEffect(() => {
     filterList();
   }, [selectedCategory, selectedSortingType, keyword, page, filterList]);
 
-  useEffect(() => {
-    checkMyParticipation();
-  }, [checkMyParticipation]);
-
   return (
-    <div className='w-full'>
-      <div className='tb2  text-text1'>스터디 My 정보</div>
-      <div className='tm2 text-text1 mt-[30px]'>내가 작성한 글</div>
-      <div className='mt-6'>
-        <ListMenuStudy
-          selectedCategory={selectedCategory}
-          setSelectedCategory={setSelectedCategory}
-          selectedSortingType={selectedSortingType}
-          setSelectedSortingType={setSelectedSortingType}
-          setKeyword={setKeyword}
-          setPage={setPage}
-        />
-      </div>
-      <div
-        className={`flex flex-col gap-[30px] mt-[30px] relative ${
-          myCommunityList.length === 0 &&
-          'h-[calc(100vh-105px-113px-193px-200px)]'
-        }`}
-      >
-        {myCommunityList.length === 0 && (
-          <div className='flex justify-center items-center absolute top-[45%] left-1/2 -translate-x-1/2 -translate-y-1/2 tm2 text-text1/80'>
-            내가 작성한 글이 없습니다.
-          </div>
-        )}
-        {myCommunityList.length !== 0 &&
-          myCommunityList.map((post) => (
-            <RecruitmentCard
-              key={post.id}
-              type='study'
-              studyId={studyId}
-              {...post}
+    <>
+      {isLoading ? (
+        <MyStudyInfoSkeleton />
+      ) : (
+        <div className='w-full'>
+          <div className='tb2  text-text1'>스터디 My 정보</div>
+          <div className='tm2 text-text1 mt-[30px]'>내가 작성한 글</div>
+          <div className='mt-6'>
+            <ListMenuStudy
+              selectedCategory={selectedCategory}
+              setSelectedCategory={setSelectedCategory}
+              selectedSortingType={selectedSortingType}
+              setSelectedSortingType={setSelectedSortingType}
+              setKeyword={setKeyword}
+              setPage={setPage}
             />
-          ))}
-      </div>
-      <div className='mt-[45px]'>
-        <Pagination
-          totalPages={totalPages}
-          currentPage={page}
-          onPageChange={setPage}
-        />
-      </div>
-
-      {!isLeader && isExtended && !isParticipated && (
-        <div className='mt-13'>
-          <div className='tm2 text-text1'>스터디 연장 참여</div>
-          <hr className='mt-4 text-border2' />
-          <div className='flex justify-between items-center mt-6 t3'>
-            <span className='text-green'>
-              연장된 스터디에 참여하실 수 있습니다.
-            </span>
-            <button
-              className='button-type2 min-w-[140px]!'
-              onClick={() => setIsOpenExtendModal(true)}
-            >
-              스터디 연장 참여
-            </button>
           </div>
-        </div>
-      )}
-      {isExtended && isParticipated && (
-        <div className='mt-13'>
-          <div className='tm2 text-text1'>스터디 연장 참여</div>
-          <hr className='mt-4 text-border2' />
-          <div className='flex justify-between items-center mt-6 t3'>
-            <span className='text-green'>
-              연장된 스터디에 참여 완료하셨습니다.
-            </span>
-            <button
-              className='button-type2 min-w-[140px]! disabled:cursor-not-allowed!'
-              disabled
-            >
-              스터디 연장 참여
-            </button>
-          </div>
-        </div>
-      )}
-      {!isExtended && (
-        <div className='mt-13'>
-          <div className='tm2 text-text1'>스터디 연장 참여</div>
-          <hr className='mt-4 text-border2' />
-          <div className='flex justify-between items-center mt-6 t3'>
-            <span className='text-green'>
-              아직 스터디가 연장되지 않았습니다.
-            </span>
-            <button
-              className='button-type2 min-w-[140px]! disabled:cursor-not-allowed!'
-              disabled
-            >
-              스터디 연장 참여
-            </button>
-          </div>
-        </div>
-      )}
-
-      <div className='mt-13'>
-        <div className='tm2 text-text1'>스터디 탈퇴</div>
-        <hr className='mt-4 text-border2' />
-        <div className='flex justify-between items-center mt-6 t3'>
-          <span className='text-red'>
-            스터디를 탈퇴하면 불이익이 발생합니다.
-          </span>
-          <button
-            className='button-type2 min-w-[140px]!'
-            onClick={() => setIsOpenOutModal(true)}
+          <div
+            className={`flex flex-col gap-[30px] mt-[30px] relative ${
+              myCommunityList.length === 0 &&
+              'h-[calc(100vh-105px-113px-193px-200px)]'
+            }`}
           >
-            스터디 탈퇴
-          </button>
-        </div>
-      </div>
+            {myCommunityList.length === 0 && (
+              <div className='flex justify-center items-center absolute top-[45%] left-1/2 -translate-x-1/2 -translate-y-1/2 tm2 text-text1/80'>
+                내가 작성한 글이 없습니다.
+              </div>
+            )}
+            {myCommunityList.length !== 0 &&
+              myCommunityList.map((post) => (
+                <RecruitmentCard
+                  key={post.id}
+                  type='study'
+                  studyId={studyId}
+                  {...post}
+                />
+              ))}
+          </div>
+          <div className='mt-[45px]'>
+            <Pagination
+              totalPages={totalPages}
+              currentPage={page}
+              onPageChange={setPage}
+            />
+          </div>
 
-      {isOpenExtendModal && (
-        <StudyExtendCheckModal
-          studyId={studyId}
-          onClose={handleCloseExtendModal}
-        />
+          {!isLeader && isExtended && !isParticipated && (
+            <div className='mt-13'>
+              <div className='tm2 text-text1'>스터디 연장 참여</div>
+              <hr className='mt-4 text-border2' />
+              <div className='flex justify-between items-center mt-6 t3'>
+                <span className='text-green'>
+                  연장된 스터디에 참여하실 수 있습니다.
+                </span>
+                <button
+                  className='button-type2 min-w-[140px]!'
+                  onClick={() => setIsOpenExtendModal(true)}
+                >
+                  스터디 연장 참여
+                </button>
+              </div>
+            </div>
+          )}
+          {isExtended && isParticipated && (
+            <div className='mt-13'>
+              <div className='tm2 text-text1'>스터디 연장 참여</div>
+              <hr className='mt-4 text-border2' />
+              <div className='flex justify-between items-center mt-6 t3'>
+                <span className='text-green'>
+                  연장된 스터디에 참여 완료하셨습니다.
+                </span>
+                <button
+                  className='button-type2 min-w-[140px]! disabled:cursor-not-allowed!'
+                  disabled
+                >
+                  스터디 연장 참여
+                </button>
+              </div>
+            </div>
+          )}
+          {!isExtended && (
+            <div className='mt-13'>
+              <div className='tm2 text-text1'>스터디 연장 참여</div>
+              <hr className='mt-4 text-border2' />
+              <div className='flex justify-between items-center mt-6 t3'>
+                <span className='text-green'>
+                  아직 스터디가 연장되지 않았습니다.
+                </span>
+                <button
+                  className='button-type2 min-w-[140px]! disabled:cursor-not-allowed!'
+                  disabled
+                >
+                  스터디 연장 참여
+                </button>
+              </div>
+            </div>
+          )}
+
+          <div className='mt-13'>
+            <div className='tm2 text-text1'>스터디 탈퇴</div>
+            <hr className='mt-4 text-border2' />
+            <div className='flex justify-between items-center mt-6 t3'>
+              <span className='text-red'>
+                스터디를 탈퇴하면 불이익이 발생합니다.
+              </span>
+              <button
+                className='button-type2 min-w-[140px]!'
+                onClick={() => setIsOpenOutModal(true)}
+              >
+                스터디 탈퇴
+              </button>
+            </div>
+          </div>
+
+          {isOpenExtendModal && (
+            <StudyExtendCheckModal
+              studyId={studyId}
+              onClose={handleCloseExtendModal}
+            />
+          )}
+          {isOpenOutModal && (
+            <StudyOutCheckModal
+              studyId={studyId}
+              onClose={handleCloseOutModal}
+            />
+          )}
+        </div>
       )}
-      {isOpenOutModal && (
-        <StudyOutCheckModal studyId={studyId} onClose={handleCloseOutModal} />
-      )}
-    </div>
+    </>
   );
 }
