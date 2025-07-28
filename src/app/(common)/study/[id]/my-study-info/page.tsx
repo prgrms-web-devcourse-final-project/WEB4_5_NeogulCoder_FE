a94c90f6d09ead4adc02ff375516b333a032a12e
yubin121 <yubin045@naver.com>
2025-07-28 09:54:40 +0900

    🐛 Fix: 데이터 페칭 코드 정리

--- src/app/(common)/study/[id]/my-study-info/page.tsx
+++ src/app/(common)/study/[id]/my-study-info/page.tsx
@@ -1,14 +1,4 @@
-'use client';
-
 import MyStudyInfoClient from '@/components/study/MyStudyInfoClient';
-import {
-  checkMyRoleInStudy,
-  // checkParticipationList,
-  checkStudyExtension,
-  fetchMyCommunityList,
-} from '@/lib/api/community';
-import { useParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
 
 // export default async function MyStudyInfo({
 //   params,
@@ -17,57 +7,18 @@ import { useEffect, useState } from 'react';
 // }) {
 export default function MyStudyInfo() {
   // const { id } = await params;
-  const { id } = useParams();
 
-  const query = {
-    page: 0,
-    pageSize: 5,
-    attributeName: 'createDateTime',
-    sort: 'DESC',
-  };
+  // const query = {
+  //   page: 0,
+  //   pageSize: 5,
+  //   attributeName: 'createDateTime',
+  //   sort: 'DESC',
+  // };
+
   // const communityListData = await fetchMyCommunityList(Number(id), query);
   // const extensionData = await checkStudyExtension(Number(id));
   // // const participationListData = await checkParticipationList(Number(id));
   // const myData = await checkMyRoleInStudy(Number(id));
 
-  const [initialMyCommunityData, setInitialMyCommunityData] = useState();
-  const [extensionData, setExtensionData] = useState();
-  const [myData, setMyData] = useState();
-  const [isLoading, setIsLoading] = useState(true);
-
-  const init = async () => {
-    const data = await fetchMyCommunityList(Number(id), query);
-    setInitialMyCommunityData(data);
-
-    const extendData = await checkStudyExtension(Number(id));
-    setExtensionData(extendData);
-
-    const roleData = await checkMyRoleInStudy(Number(id));
-    setMyData(roleData);
-
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    init();
-  }, []);
-
-  // const initialMyCommunityData = communityListData || responseType;
-  console.log('initialMyCommunityData:', initialMyCommunityData);
-  console.log('extensionData:', extensionData);
-  console.log('myData:', myData);
-
-  if (isLoading) {
-    return <></>;
-  }
-
-  return (
-    <MyStudyInfoClient
-      initialMyCommunityData={initialMyCommunityData!}
-      extensionData={extensionData!}
-      // isExtended={extensionData!.extended}
-      // isLeader={myData!.role === 'LEADER'}
-      myData={myData!}
-    />
-  );
+  return <MyStudyInfoClient />;
 }
