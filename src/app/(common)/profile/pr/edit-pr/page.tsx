591327c5732cb0d7d6abf48d5713887ae03f3aa9
yubin121 <yubin045@naver.com>
2025-07-28 14:33:52 +0900

    🐛 Fix: 서버에서 prerender 방지

--- src/app/(common)/profile/pr/edit-pr/page.tsx
+++ src/app/(common)/profile/pr/edit-pr/page.tsx
@@ -1,4 +1,12 @@
-import EditPrClient from '@/components/profile/pr/EditPrClient';
+// import EditPrClient from '@/components/profile/pr/EditPrClient';
+import dynamic from 'next/dynamic';
+
+const EditPrClient = dynamic(
+  () => import('@/components/profile/pr/EditPrClient'),
+  {
+    ssr: false,
+  }
+);
 
 export default function page() {
   return (
