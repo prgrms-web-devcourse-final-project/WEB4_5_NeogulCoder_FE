4b16dc504c25bd1c800052c225aae6e37b2483bb
Chosojeong <chosojeong97@gmail.com>
2025-07-30 09:43:45 +0900

    🐛 Fix : 출석 화면 날짜 누락 수정

--- src/components/study-room/dashboard/StudyAttendance.tsx
+++ src/components/study-room/dashboard/StudyAttendance.tsx
@@ -114,7 +114,9 @@ export default function StudyAttendance({
                   events={events}
                   validRange={{
                     start: dayjs(studyInfo?.startDate).format('YYYY-MM-DD'),
-                    end: dayjs(studyInfo?.endDate).format('YYYY-MM-DD'),
+                    end: dayjs(studyInfo?.endDate).format(
+                      'YYYY-MM-DD HH:mm:ss'
+                    ),
                   }}
                   headerToolbar={{
                     left: 'prev',
