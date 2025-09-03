0cab450f973d9603597e8929ef2116a30a4d620d
sduddla <sduddla921@gmail.com>
2025-09-03 23:07:15 +0900

    💄 Style: 사이드바 반응형 적용

--- src/app/(common)/profile/layout.tsx
+++ src/app/(common)/profile/layout.tsx
@@ -1,5 +1,4 @@
-// import MobileHeader from '@/components/common/MobileHeader';
-import SideBar from '@/components/profile/ProfileSideBar';
+import ProfileSideBar from '@/components/profile/ProfileSideBar';
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
@@ -7,7 +6,7 @@ export default function layout({ children }: { children: React.ReactNode }) {
       <div className='w-full mx-auto lg:grid lg:grid-cols-[300px_1fr] lg:gap-10'>
         <aside className='hidden lg:block'>
           <div className='w-[300px]'>
-            <SideBar />
+            <ProfileSideBar />
           </div>
         </aside>
 
